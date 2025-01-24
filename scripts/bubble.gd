b7ae211386078322fc7ad7e96d9baf02e450bf8d
Max Stenlund <Prezzo55@gmail.com>
2025-01-24 22:57:08 +0100

    bubble is creatable

---
+++ scripts/bubble.gd
@@ -0,0 +1,32 @@
+class_name Bubble
+extends Area3D
+
+@onready var mesh = %BubbleMesh
+
+var tween : Tween
+var power: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+static func create(parent: Node, global_position: Vector3) -> Bubble:
+	var bubble = preload("res://scenes/bubble.tscn").instantiate()
+	parent.add_child(bubble)
+	bubble.global_position = global_position
+	
+	bubble.tween = bubble.create_tween()
+	bubble.tween.tween_property(bubble.mesh, "scale", Vector3(1, 1, 1), 4)
+	
+	return bubble	
+
+func release() -> float:
+	queue_free()
+	return power
+	
+func cancel():
+	queue_free()
