33273c45de60db09c3fb860e7e63875c7d1dca96
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-01-25 00:07:36 +0100

    mess cleanup

--- scripts/SignalBuss.gd
+++ scripts/SignalBuss.gd
@@ -10,11 +10,3 @@ signal player_died()
 signal bubble_release(power: float)
 #signalbuss for signals between objects
 signal _can_grab
-
-#signalbuss for signals between objects
-signal level_ready(level: Level)
-
-signal checkpoit_triggered(checkpoint: Checkpoint)
-
-signal spawn_player(spawn_point: SpawnPoint)
-signal player_died()
