1662770c254855ffd9f0b63c541fe4a3c86afe0d
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-01-25 00:04:55 +0100

    game loop initialization

--- scripts/SignalBuss.gd
+++ scripts/SignalBuss.gd
@@ -1,5 +1,12 @@
 extends Node
 
+#signalbuss for signals between objects
+signal level_ready(level: Level)
+
+signal checkpoit_triggered(checkpoint: Checkpoint)
+
+signal spawn_player(spawn_point: SpawnPoint)
+signal player_died()
 signal bubble_release(power: float)
 #signalbuss for signals between objects
 signal _can_grab
