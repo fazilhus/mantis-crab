4895c64732f4d6071ad5996486180f2f0a790800
Max Stenlund <Prezzo55@gmail.com>
2025-01-25 12:51:09 +0100

    player moves by bubble

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const AIR_RESISTANCE = 2
+const AIR_RESISTANCE = 0.3
 const STAMINA_MAX = 3
 
 var shoot: Vector3
@@ -21,10 +21,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	if can_grab and Input.is_action_just_pressed("Grab"):
 		grab()
@@ -44,10 +40,6 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
-	
-	move_and_slide()
-	camera_gimble.position = self.position
-
 func _unhandled_input(event: InputEvent):
 	pass
 
@@ -56,7 +48,7 @@ func _can_grab():
 	
 func bubble_logic():
 	if Input.is_action_just_pressed("ui_accept") and stamina > 1:
-		bubble = Bubble.create(self, stamina)
+		bubble = Bubble.create(%BubbleMarker, stamina)
 		stamina -= 1
 	if Input.is_action_just_released("ui_accept") and bubble != null:
 		bubble.release()
@@ -69,8 +61,11 @@ func bubble_logic():
 	shoot.y = move_toward(shoot.y, 0, AIR_RESISTANCE)
 	shoot.z = move_toward(shoot.z, 0, AIR_RESISTANCE)
 	
+	%Model.global_position = global_position
+	
 func on_bubble_release(power: float):
-	shoot = Vector3(0,1,1) * power
+	shoot = (%BubbleMarker.global_position - global_position) * power
+	print(shoot)
 
 func grab():
 	#TODO tween a swinging motion and apply force
