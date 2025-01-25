4919397e7b244834f396bc12d51f8007779eff03
PoC <103537260+PondOfCode@users.noreply.github.com>
2025-01-25 17:56:05 +0100

    Fixed collectable

--- scenes/collectable.gd
+++ scenes/collectable.gd
@@ -4,12 +4,11 @@ extends Area3D
 func _process(delta: float) -> void:
 	rotate_y(0.01)
 
-
 func _on_body_entered(body: Node3D) -> void:
 	$collectablesound.play()
-	$MeshInstance3D.queue_free()
-
+	%MeshInstance3D.visible = false
+	$CollisionShape3D.set_deferred("disabled", true) 
 
 func _on_collectablesound_finished() -> void:
-	SignalBuss.collected_collectable.emit(self)
+	SignalBuss.collected_collectable.emit()
 	queue_free()
