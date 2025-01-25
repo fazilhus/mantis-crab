3d791ee7e2174a24c0fe4ffc310b76b0bac00062
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-01-25 20:24:37 +0100

    gui tied up

--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -1,28 +1,15 @@
 extends Control
 
-@export var options_menu_pkd : PackedScene
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
+func _on_play_button_2_pressed():
+	GUIBuss.play_button_pressed.emit()
 
 func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+	GUIBuss.quit_game.emit()
 
 
-func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file(options_menu_pkd.resource_path)
+func _on_options_button_2_pressed():
+	GUIBuss.menu_options_button_pressed.emit()
 
 
-func _on_chapter_button_pressed() -> void:
-	get_tree().change_scene()
+func _on_quit_button_2_pressed():
+	GUIBuss.quit_game.emit()
