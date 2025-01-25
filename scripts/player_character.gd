914f1de3665cbc8de4c0e51621ef966e7611ee6d
TheCoolStuntman <sebastian.ef.fransson@gmail.com>
2025-01-25 18:30:11 +0100

    Movement almost fixed

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const AIR_RESISTANCE = 0.3
+const AIR_RESISTANCE = 0.5
 const STAMINA_MAX = 3
 
 var shoot: Vector3
@@ -18,26 +18,22 @@ func _ready():
 	SignalBuss.bubble_release.connect(on_bubble_release)
 
 func _physics_process(delta: float) -> void:
+	bubble_logic()
+		
+	if is_on_floor():
+		ground_movement(delta)
+	else:
+		air_movement(delta)
+	
+	print(velocity)
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
 
 	if can_grab and Input.is_action_just_pressed("Grab"):
 		grab()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
-	bubble_logic()
-	
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent):
@@ -47,10 +43,10 @@ func _can_grab():
 	can_grab = true
 	
 func bubble_logic():
-	if Input.is_action_just_pressed("ui_accept") and stamina > 1:
+	if Input.is_action_just_pressed("Create_Bubble_Action") and stamina > 1:
 		bubble = Bubble.create(%BubbleMarker, stamina)
 		stamina -= 1
-	if Input.is_action_just_released("ui_accept") and bubble != null:
+	if Input.is_action_just_released("Create_Bubble_Action") and bubble != null:
 		bubble.release()
 		bubble = null
 	if is_on_floor():
@@ -61,8 +57,6 @@ func bubble_logic():
 	shoot.y = move_toward(shoot.y, 0, AIR_RESISTANCE)
 	shoot.z = move_toward(shoot.z, 0, AIR_RESISTANCE)
 	
-	%Model.global_position = global_position
-	
 func on_bubble_release(power: float):
 	shoot = (%BubbleMarker.global_position - global_position) * power
 	print(shoot)
@@ -70,3 +64,29 @@ func on_bubble_release(power: float):
 func grab():
 	#TODO tween a swinging motion and apply force
 	pass
+
+func air_movement(delta):
+	velocity += get_gravity() * delta
+	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x += direction.x * SPEED / 2
+		velocity.z += direction.z * SPEED / 2
+	
+	if direction.x * velocity.x < 0 :
+		pass
+	
+func ground_movement(delta):
+	if Input.is_action_just_pressed("Jump_Action"):
+		velocity.y += 10
+	
+	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, 2) #sides
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, 2) #forward & backward
+	else:
+		velocity.x = move_toward(velocity.x, 0, 1)
+		velocity.z = move_toward(velocity.z, 0, 1)
+	
