2b371052e25d65b04bf5a645ffc93208b0c7e72f
Max Stenlund <Prezzo55@gmail.com>
2025-01-25 11:18:44 +0100

    Bubble creation for character

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -14,19 +14,23 @@ func _ready():
 func _process(_delta):
 	pass
 
-static func create(parent: Node, global_position: Vector3, chargeRate: float) -> Bubble:
+static func create(bubblePosition: Node, chargeRate: float) -> Bubble:
 	var bubble = preload("res://scenes/bubble.tscn").instantiate()
-	parent.add_child(bubble)
-	bubble.global_position = global_position
+	bubblePosition.add_child(bubble)
+	bubble.global_position = bubblePosition.global_position
 	
 	bubble.tween = bubble.create_tween()
+	
 	bubble.tween.tween_property(bubble.mesh, "scale", Vector3(1, 1, 1), 1/chargeRate)
+	bubble.tween.parallel().tween_property(bubble, "power", 5, 1/chargeRate)
+	
 	
 	return bubble	
 
 func release():
 	SignalBuss.bubble_release.emit(power)
-	queue_free()
+	get_parent().remove_child(self)
+	
 	
 func cancel():
 	queue_free()
--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -3,19 +3,22 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const AIR_RESISTANCE = 2
+const STAMINA_MAX = 3
+
+var shoot: Vector3
+var bubble: Bubble
+var stamina: int = STAMINA_MAX
 var is_grabbing : bool = false
 var can_grab : bool = false
 func _ready():
 	SignalBuss._can_grab.connect(_can_grab)
+	SignalBuss.bubble_release.connect(on_bubble_release)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -27,8 +30,26 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
 	
 func _can_grab():
 	can_grab = true
+	
+func bubble_logic():
+	if Input.is_action_just_pressed("ui_accept") and stamina > 1:
+		bubble = Bubble.create(self, stamina)
+		stamina -= 1
+	if Input.is_action_just_released("ui_accept") and bubble != null:
+		bubble.release()
+		bubble = null
+	if is_on_floor():
+		stamina = STAMINA_MAX
+	
+	velocity += shoot
+	shoot.x = move_toward(shoot.x, 0, AIR_RESISTANCE)
+	shoot.y = move_toward(shoot.y, 0, AIR_RESISTANCE)
+	shoot.z = move_toward(shoot.z, 0, AIR_RESISTANCE)
+	
+func on_bubble_release(power: float):
+	shoot = Vector3(0,1,1) * power
