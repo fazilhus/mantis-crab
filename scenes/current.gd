68fe86627af72132a77fbd7c102dc6975fbeaf67
Virrmanskii <lundbladvictor@gmail.com>
2025-01-25 12:16:18 +0100

    functioning currents. added inputmaps

--- scenes/current.gd
+++ scenes/current.gd
@@ -1,6 +1,9 @@
 extends Area3D
+class_name current
 
-
+var entered : bool = false
+var exited : bool = false
+var player : PlayerCharacter
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,15 +11,33 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
-	pass
+	if entered:
+		player.velocity.x = move_toward(player.velocity.x, 8, 4)
+		player.move_and_slide()
+	if exited:
+		player.velocity.x = move_toward(player.velocity.x, -8, 4)
+		player.move_and_slide()
+		exited = false
+		
+	
+	
 
 
 
 func _on_body_exited(body:Node3D):
 	if body is PlayerCharacter:
-		body.apply_force(Vector3(500,0,0), body.global_position)
+		player = body
+		entered = false
+		exited = true
+		print("should move")
+		#body.apply_force(Vector3(500,0,0), body.global_position)
 
 
 func _on_body_entered(body:Node3D):
 	if body is PlayerCharacter:
-		body.apply_force(Vector3(-500,0,0), body.global_position)
+		player = body
+		entered = true
+		print("should move")
+		# body.velocity.x = move_toward(body.velocity.x, 5, 2)
+		# body.move_and_slide()
+		#body.apply_force(Vector3(-500,0,0), body.global_position)
--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -5,6 +5,8 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var is_grabbing : bool = false
 var can_grab : bool = false
+@onready var camera_gimble = get_node("CameraGimble_Node3D")
+
 func _ready():
 	SignalBuss._can_grab.connect(_can_grab)
 
@@ -17,9 +19,12 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if can_grab and Input.is_action_just_pressed("Grab"):
+		grab()
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -28,7 +33,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide()
 	
+	move_and_slide()
+	camera_gimble.position = self.position
+
+func _unhandled_input(event: InputEvent):
+	pass
+
 func _can_grab():
 	can_grab = true
+
+func grab():
+	#TODO tween a swinging motion and apply force
+	pass
