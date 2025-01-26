38329d7adcf48ee0b266e06abe5342dc9a0e6c26
Virrmanskii <lundbladvictor@gmail.com>
2025-01-26 12:07:29 +0100

    added swoosh sounds for current

--- scenes/current.gd
+++ scenes/current.gd
@@ -21,15 +21,13 @@ func _process(delta: float):
 
 func _on_body_exited(body:Node3D):
 	if body is PlayerCharacter:
-		audio_streamer.play(0)
+		
 		player = body
 		SignalBuss.current_entered.emit(Vector3.ZERO)
 
 
 func _on_body_entered(body:Node3D):
 	if body is PlayerCharacter:
+		audio_streamer.play(0)
 		player = body
 		SignalBuss.current_entered.emit(current_force_multiplier * (stream_End_marker.global_position-player.global_position).normalized())
-	# 	entered = true
-	# 	exited = false
-	# 	current_force_direction = 0.2*(stream_End_marker.global_position-player.global_position).normalized()
