09468fbbd999063f52f6d5ca85bc33d3aede12e4
TheCoolStuntman <sebastian.ef.fransson@gmail.com>
2025-01-26 14:20:51 +0100

    coral break fx

--- scripts/coral.gd
+++ scripts/coral.gd
@@ -1,19 +1,27 @@
 extends Node3D
 class_name Coral
 
+@onready var fx = $Node3D/GPUParticles3D
+var timer : Timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	timer = Timer.new()
+	add_child(timer)
+	timer.autostart = false
+	#timer.timeout.connect(self.queue_free)
 
 func _on_area_3d_body_entered(body:Node3D):
 		#TODO Update statement to check for bubble
 	if body.get_parent() is PlayerCharacter:
 		#Break animation
 		#Break sound
-		self.queue_free()
+		destroy(0)
 
 func destroy(power: float):
-	queue_free()
+	$MeshInstance3D.visible = false
+	fx.emitting = true
+	timer.start(fx.lifetime)
+	%CrunchSound.play()
 
 func _on_snap_area_3d_area_entered(area):
 	if area is Bubble:
