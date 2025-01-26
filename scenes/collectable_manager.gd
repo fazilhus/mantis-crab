e47c78490ae2b7485528b62e15b66c7ff0a4bfd1
PoC <103537260+PondOfCode@users.noreply.github.com>
2025-01-26 14:50:39 +0100

    Fixed collectables

--- scenes/collectable_manager.gd
+++ scenes/collectable_manager.gd
@@ -4,7 +4,12 @@ var amount_collected: int
 	
 func _ready() -> void:
 	SignalBuss.collected_collectable.connect(handle_collectables)
+	SignalBuss.level_ready.connect(reset_collectables)
 
 func handle_collectables() -> void:
 	amount_collected = amount_collected + 1
+	SignalBuss.emit_signal("collected_amount", amount_collected)
+	
+func reset_collectables() -> void:
+	amount_collected = 0
 	SignalBuss.emit_signal("collected_amount", amount_collected)
