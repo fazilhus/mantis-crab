30f2f80e36a00f6e9155d1ba2b694c94dc7ff4be
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-01-26 11:49:57 +0100

    mouse capture bug after death fix

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -69,25 +69,36 @@ func _show_main_menu() -> void:
 	menu.default_focus.grab_focus()
 	options.hide()
 	pause.hide()
+	_capture_mouse(false)
 
 func _hide_main_menu() -> void:
 	menu.hide()
 	options.hide()
 	pause.hide()
+	_capture_mouse(true)
 
 func _show_options_menu() -> void:
 	menu.hide()
 	options.show()
 	options.default_focus.grab_focus()
 	pause.hide()
+	_capture_mouse(false)
 
 func _show_pause_menu() -> void:
 	menu.hide()
 	options.hide()
 	pause.show()
 	pause.default_focus.grab_focus()
+	_capture_mouse(false)
 
 func _hide_pause_menu() -> void:
 	menu.hide()
 	options.hide()
 	pause.hide()
+	_capture_mouse(true)
+
+func _capture_mouse(val: bool) -> void:
+	if val:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
