78a54d78d6e9d24a7237739cb547ed29d1085350
TheCoolStuntman <sebastian.ef.fransson@gmail.com>
2025-01-26 15:03:05 +0100

    ni vet

--- scripts/coral.gd
+++ scripts/coral.gd
@@ -1,14 +1,14 @@
 extends Node3D
 class_name Coral
 
-@onready var fx = $Node3D/GPUParticles3D
+@onready var fx = %GPUParticles3D
 var timer : Timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer = Timer.new()
 	add_child(timer)
 	timer.autostart = false
-	#timer.timeout.connect(self.queue_free)
+	timer.timeout.connect(self.queue_free)
 
 func _on_area_3d_body_entered(body:Node3D):
 		#TODO Update statement to check for bubble
