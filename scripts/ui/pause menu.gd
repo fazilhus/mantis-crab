3dd3c6e47396e619f52311fdeaaf49afef886302
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-01-26 10:06:40 +0100

    menu scaling fixed

--- scripts/ui/pause menu.gd
+++ scripts/ui/pause menu.gd
@@ -1,7 +1,7 @@
 class_name PauseMenu
 extends Control
 
-@onready var default_focus : Control = $Panel/Resume
+@onready var default_focus : Control = %Resume
 
 func _on_resume_pressed() -> void:
 	GUIBuss.pause_menu_resume_button_pressed.emit()
