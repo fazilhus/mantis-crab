48609da2211773af7253ae9a16a2c95749f1befa
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-01-26 13:07:25 +0100

    UI do be UIing

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,6 +4,8 @@ extends CanvasLayer
 @onready var menu : MainMenu = %Menu
 @onready var options : OptionsMenu = %Options
 @onready var pause : PauseMenu = %Pause
+@onready var credits : CreditsMenu = %Credits
+@onready var buttons : ButtonMapMenu = %ButtonMap
 @onready var death_screen = %Control
 
 enum OptionsPrevious {MAIN, PAUSE}
@@ -12,8 +14,12 @@ var timer : Timer
 
 func _ready() -> void:
 	GUIBuss.game_started.connect(_on_game_started)
+
 	GUIBuss.level_started.connect(_on_level_started)
 	GUIBuss.menu_options_button_pressed.connect(_on_menu_options_button_pressed)
+	GUIBuss.menu_credits_button_pressed.connect(_on_menu_credits_button_pressed)
+	GUIBuss.menu_buttons_button_pressed.connect(_on_menu_buttons_button_pressed)
+
 	GUIBuss.option_menu_back_button_pressed.connect(_on_option_menu_back_button_pressed)
 	GUIBuss.pause_game.connect(_on_pause_game)
 
@@ -34,11 +40,14 @@ func _on_player_spawned():
 	timer.queue_free()
 	death_screen.hide()
 
+	GUIBuss.credits_back_pressed.connect(_on_credits_back_pressed)
+	GUIBuss.buttons_back_pressed.connect(_on_buttons_back_pressed)
+
 func _on_game_started() -> void:
 	_show_main_menu()
 
 func _on_level_started() -> void:
-	_hide_main_menu()
+	_hide_menu()
 
 func _on_pause_game() -> void:
 	_show_pause_menu()
@@ -46,6 +55,12 @@ func _on_pause_game() -> void:
 func _on_menu_options_button_pressed() -> void:
 	options_previous = OptionsPrevious.MAIN
 	_show_options_menu()
+
+func _on_menu_credits_button_pressed() -> void:
+	_show_credits_menu()
+
+func _on_menu_buttons_button_pressed() -> void:
+	_show_buttons_menu()
 
 func _on_option_menu_back_button_pressed() -> void:
 	match options_previous:
@@ -55,7 +70,7 @@ func _on_option_menu_back_button_pressed() -> void:
 			_show_pause_menu()
 
 func _on_pause_menu_resume_button_pressed() -> void:
-	_hide_pause_menu()
+	_hide_menu()
 
 func _on_pause_menu_options_button_pressed() -> void:
 	options_previous = OptionsPrevious.PAUSE
@@ -64,17 +79,28 @@ func _on_pause_menu_options_button_pressed() -> void:
 func _on_pause_menu_home_button_pressed() -> void:
 	_show_main_menu()
 
+func _on_credits_back_pressed() -> void:
+	_show_main_menu()
+
+func _on_buttons_back_pressed() -> void:
+	_show_main_menu()
+
+
 func _show_main_menu() -> void:
 	menu.show()
 	menu.default_focus.grab_focus()
 	options.hide()
 	pause.hide()
+	credits.hide()
+	buttons.hide()
 	_capture_mouse(false)
 
-func _hide_main_menu() -> void:
+func _hide_menu() -> void:
 	menu.hide()
 	options.hide()
 	pause.hide()
+	credits.hide()
+	buttons.hide()
 	_capture_mouse(true)
 
 func _show_options_menu() -> void:
@@ -82,6 +108,26 @@ func _show_options_menu() -> void:
 	options.show()
 	options.default_focus.grab_focus()
 	pause.hide()
+	credits.hide()
+	buttons.hide()
+	_capture_mouse(false)
+
+func _show_credits_menu() -> void:
+	menu.hide()
+	options.hide()
+	pause.hide()
+	credits.show()
+	credits.default_focus.grab_focus()
+	buttons.hide()
+	_capture_mouse(false)
+
+func _show_buttons_menu() -> void:
+	menu.hide()
+	options.hide()
+	pause.hide()
+	credits.hide()
+	buttons.show()
+	buttons.default_focus.grab_focus()
 	_capture_mouse(false)
 
 func _show_pause_menu() -> void:
@@ -90,12 +136,6 @@ func _show_pause_menu() -> void:
 	pause.show()
 	pause.default_focus.grab_focus()
 	_capture_mouse(false)
-
-func _hide_pause_menu() -> void:
-	menu.hide()
-	options.hide()
-	pause.hide()
-	_capture_mouse(true)
 
 func _capture_mouse(val: bool) -> void:
 	if val:
---
+++ scripts/ui/credits.gd
@@ -0,0 +1,11 @@
+class_name CreditsMenu
+extends Control
+
+@onready var default_focus : Control = %Back
+
+func _unhandled_input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
+		_on_back_pressed()
+
+func _on_back_pressed():
+	GUIBuss.credits_back_pressed.emit()
